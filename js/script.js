e60f884a65b0e88ba581373a76d295b7fe00dfad
Mihail <mihailgerasimov86@gmail.com>
2021-05-26 18:16:42 +0400

    f

--- js/script.js
+++ js/script.js
@@ -1,23 +1,6 @@
-'use struct';
+"use strict";
 
-const kl = new Swiper('.services-container', {
-
-  navigation: {
-    nextEl: '.cases-next',
-    prevEl: '.cases-prev',
-  },
-  // centeredSlides:true,
-
-  // slidesPerView: 4.3,
-  // spaceBetween: 0,
-  // simulateTouch:false,
-  // autoplay: {
-  //     delay: 1000,
-  //   },
-  //  slidesPerGroup:1
-});
-
-const swiper = new Swiper('.swiper-container', {
+const swiper = new Swiper('.image-slider', {
 
     navigation: {
       nextEl: '.swiper-button-next',
@@ -35,3 +18,116 @@ const swiper = new Swiper('.swiper-container', {
       },
    
   });
+
+
+const swiperR = new Swiper('.services-container', {
+
+  navigation: {
+    nextEl: '.services-next',
+    prevEl: '.services-prev',
+  },
+
+  breakpoints: {
+    1400: {
+      slidesPerView:4.3,
+    },
+    1220: {
+        slidesPerView:3.8,
+    },
+    1020: {
+      slidesPerView:3.1,
+  },
+  860: {
+    slidesPerView:3.1,
+},
+700: {
+  slidesPerView:2.5,
+},
+576: {
+  slidesPerView:2,
+},
+475: {
+  slidesPerView:1.6,
+},
+426: {
+  slidesPerView:1.4,
+}
+  },
+
+});
+
+let radius = 275;
+let autoRotate = true;
+let rotateSpeed = -100;
+let imgWidth = 140;
+let imgHeight = 205;
+setTimeout(init, 300);
+let odrag = document.getElementById("drag-container");
+let ospin = document.getElementById("spin-container");
+let carousel = document.getElementById("carousel");
+let aImg = ospin.getElementsByTagName("a");
+ospin.style.width = imgWidth + "px";
+ospin.style.height = imgHeight + "px";
+let ground = document.getElementById("ground");
+ground.style.width = radius * 3 + "px";
+ground.style.height = radius * 3 + "px";
+
+function init(delayTime) {
+    for (let i = 0; i < aImg.length; i++) {
+        aImg[i].style.transform =
+            "rotateY(" +
+            i * (360 / aImg.length) +
+            "deg) translateZ(" +
+            radius +
+            "px)";
+    }
+}
+
+function applyTranform(obj) {
+    if (tY > 0) tY = 0;
+    if (tY < 0) tY = 0;
+    obj.style.transform = "rotateX(" + -tY + "deg) rotateY(" + tX + "deg)";
+}
+let sX,
+    sY,
+    nX,
+    nY,
+    desX = 0,
+    desY = 0,
+    tX = 0,
+    tY = 0;
+if (autoRotate) {
+    let animationName = rotateSpeed > 0 ? "spin" : "spinRevert";
+    ospin.style.animation = `${animationName} ${Math.abs(
+    rotateSpeed
+    )}s infinite linear`;
+}
+carousel.onpointerdown = function(e) {
+    clearInterval(odrag.timer);
+    e = e || window.event;
+    let sX = e.clientX,
+        sY = e.clientY;
+    this.onpointermove = function(e) {
+        e = e || window.event;
+        let nX = e.clientX,
+            nY = e.clientY;
+        desX = nX - sX;
+        desY = nY - sY;
+        tX += desX * 0.1;
+        tY += desY * 0.1;
+        applyTranform(odrag);
+        sX = nX;
+        sY = nY;
+    };
+    this.onpointerup = function(e) {
+        odrag.timer = setInterval(function() {
+            desX *= 0.95;
+            desY *= 0.95;
+            tX += desX * 0.1;
+            tY += desY * 0.1;
+            applyTranform(odrag);
+        }, 17);
+        this.onpointermove = this.onpointerup = null;
+    };
+    return false;
+};
